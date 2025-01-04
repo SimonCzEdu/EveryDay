b14828ddf0913f856e9bb2d1a67843e5bef1e7ea
Szymon Czapiewski <simonczedu@gmail.com>
2025-01-04 13:11:59 +0000

    Add: Products app & its models

---
+++ products/admin.py
@@ -0,0 +1,7 @@
+""" Register models for admin panel """
+from django.contrib import admin
+from .models import Product, Category
+
+# Register your models here.
+admin.site.register(Product)
+admin.site.register(Category)
